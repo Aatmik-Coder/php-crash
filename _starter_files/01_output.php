2587c4bafd1a8cec69fbeb8e6772363a231f8862
mike <coderaatmik@gmail.com>
2022-09-25 10:08:08 +0530

    file one edited

--- _starter_files/01_output.php
+++ _starter_files/01_output.php
@@ -4,3 +4,18 @@
 /*
   A .php file running on a server, can output both HTML and PHP code. There are multiple functions that can be used to output data to the browser.
 */
+  echo "Hello world mike sanghvi";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+  <body>
+    <h1><?php echo 123; ?></h1>  
+  </body>
+</html>
