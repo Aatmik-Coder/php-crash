21e6751837e27cadac3735a9eaf00b47526f27a4
mike <coderaatmik@gmail.com>
2022-09-26 10:38:24 +0530

    learned about till sanitizing inputs

--- _starter_files/10_get_post.php
+++ _starter_files/10_get_post.php
@@ -4,3 +4,20 @@
 /*
   We can pass data through urls and forms using the $_GET and $_POST superglobals.
 */
+echo $_POST['name'];
+echo $_POST['age'];
+?>
+
+<a href="<?php echo $_SERVER['PHP_SELF']; ?>?name=aatmik">Click</a>
+
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+  <div>
+    <label for="name">Name</label>  
+    <input type="text" name="name">
+  </div>
+  <div>
+    <label for="age">Age:</label>
+    <input type="text" name="age">
+  </div>
+  <input type="submit" value="submit" name="submit">
+</form>
--- _starter_files/11_sanitizing_inputs.php
+++ _starter_files/11_sanitizing_inputs.php
@@ -4,3 +4,28 @@
 /*
   Data submitted through a form is not sanitized by default. We have methods to sanitize data manually.
 */
+// echo $_POST['age'];
+
+if(isset($_POST['submit'])) {
+  $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+  
+  $age = filter_input(INPUT_POST, 'age', FILTER_SANITIZE_SPECIAL_CHARS);
+  
+  echo $_POST['name'];
+
+  }
+?>
+
+<!-- <a href="<?php echo $_SERVER['PHP_SELF']; ?>?name=aatmik">Click</a> -->
+
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+  <div>
+    <label for="name">Name</label>  
+    <input type="text" name="name">
+  </div>
+  <div>
+    <label for="age">Age:</label>
+    <input type="text" name="age">
+  </div>
+  <input type="submit" value="submit" name="submit">
+</form>
